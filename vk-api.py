d7c642432175865d0d3eba4513db56ffcb5c3f50
Sergievsky Nikita <reetray@protonmail.com>
2021-04-17 14:43:29 +0300

    Я решил эту проблему / Перешел на VkWave

--- vk-api.py
+++ vk-api.py
@@ -4,34 +4,34 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 from loguru import logger
-import tgfuncs
 import asyncio
-
-# 1779702753:AAFPtwDg98ir8CK6f-2HVOky0rcSTeDtheE
-# 9e8b113868e44cd7ea23716f7b4e4bc1d2f90a33c953d22c49a828995a6b95b196190c5588ea3b31f
-# 198731493
+import tgfuncs
 
 logger.info('Started')
 
-bot = Bot(token='1779702753:AAFPtwDg98ir8CK6f-2HVOky0rcSTeDtheE')
+bot = Bot(token='')
 logger.debug('Telegram: got api')
 
 dp = Dispatcher(bot)
 tgfuncs.setup_handlers(dp)
-logger.debug('Telegram: dispachers ready')
 
-bot = SimpleLongPollBot(tokens="9e8b113868e44cd7ea23716f7b4e4bc1d2f90a33c953d22c49a828995a6b95b196190c5588ea3b31fd074", group_id=198731493)
+logger.debug('Telegram: dispachers ready')
+bot2 = SimpleLongPollBot(tokens="", group_id=)
 logger.debug('VKAPI: registered bot')
 
-@bot.message_handler()
-async def handle(event: bot.SimpleBotEvent):
+@bot2.message_handler()
+async def handle(event: bot2.SimpleBotEvent):
     await event.answer("hello world!")
 
 logger.debug('VKAPI: handlers ready')
 
-def start():
+def start_polling_tg():
     executor.start_polling(dp)
     logger.debug('Polling Telegram')
-    bot.run_forever()
-    logger.debug('Polling vk')
-start()
+
+async def start_polling_vk():
+    await bot2.run()
+    logger.debug('Polling Vk')
+
+dp.loop.create_task(start_polling_vk())
+executor.start_polling(dp, skip_updates=True)
