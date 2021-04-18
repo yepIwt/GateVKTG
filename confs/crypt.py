fceb6df0e40d4e38079cd20dcbb537177abb7a1d
Sergievsky Nikita <reetray@protonmail.com>
2021-04-18 13:41:23 +0300

    Добавил поддержку шифрования под Windows

--- confs/crypt.py
+++ confs/crypt.py
@@ -1,33 +1,33 @@
+#v.2
 from Crypto.Cipher import AES
 import hashlib
+import os
 
 class LetItCrypt(object):
 
-	__slots__ = ('key','mode','IV')
+	def __init__(self, passw: str):
+		self.key = hashlib.sha256(passw.encode()).digest()
+		self.mode = AES.MODE_EAX
 
-	def __init__(self,passw):
-		self.key = hashlib.sha256(passw.encode('utf-8')).digest()
-		self.mode = AES.MODE_CBC
-		self.IV = 'This is an IV456'
+	def pad(self, text: bytes) -> bytes:
+		while len(text) % 16 != 0:
+			text += b' '
+		return text
 
-	def pad_message(self,message):
-		while len(message) % 16 != 0:
-			message = message + b' '
-		return message
+	def enc_cfg(self, data: str) -> None:
+		cipher = AES.new(self.key, self.mode)
+		ciphertext, tag = cipher.encrypt_and_digest(data.encode())
+		file_out = open("data", "wb")
+		[ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+		file_out.close()
 
-	def enc(self,data):
-		cipher = AES.new(self.key, self.mode, self.IV)
-		padded_message = self.pad_message(data.encode())
-		encrypted = cipher.encrypt(padded_message)
-		with open('data','wb') as f:
-			f.write(encrypted)
-
-	def dec(self):
-		cipher = AES.new(self.key, self.mode, self.IV)
-		with open('data','rb') as f:
-			data = f.read()
-		return cipher.decrypt(data).rstrip().decode()
-
-#a = LetItCrypt('123')
-#a.enc('hey you. open your eyes')
-#print(a.dec())
+	def dec_cfg(self) -> str:
+		file_in = open("data", "rb")
+		nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
+		cipher = AES.new(self.key, self.mode, nonce)
+		try:
+			data = cipher.decrypt_and_verify(ciphertext, tag)
+		except ValueError:
+			return ''
+		else:
+			return data
