4a8fce7b74b443e10f38009fc566e270c6a21e36
Nikita Sergievsky <n.serg1evsky@gmail.com>
2021-04-19 21:03:55 +0300

    Добавил отправку сообщения из Вк в Тг. Важно: я использую aiogram==2.9

--- async-fix.py
+++ async-fix.py
@@ -7,31 +7,33 @@ from loguru import logger
 import asyncio
 import tgfuncs
 
+import confs
+c = confs.Config()
+c.unlock_file('')
+
 logger.info('Started')
 
-bot = Bot(token='')
+bot = Bot(token=c.data['tg']['token'])
 logger.debug('Telegram: got api')
 
 dp = Dispatcher(bot)
 tgfuncs.setup_handlers(dp)
 
 logger.debug('Telegram: dispachers ready')
-bot2 = SimpleLongPollBot(tokens="", group_id=)
+bot2 = SimpleLongPollBot(tokens=c.data['vk']['public_token'], group_id=c.data['vk']['public_id'])
 logger.debug('VKAPI: registered bot')
 
 @bot2.message_handler()
 async def handle(event: bot2.SimpleBotEvent):
-    await event.answer("hello world!")
+    #await event.answer("hello world!")
+    await bot.send_message("", event.object.object.message.text)
 
 logger.debug('VKAPI: handlers ready')
-
-def start_polling_tg():
-    executor.start_polling(dp)
-    logger.debug('Polling Telegram')
 
 async def start_polling_vk():
     await bot2.run()
     logger.debug('Polling Vk')
 
 dp.loop.create_task(start_polling_vk())
+logger.debug('Polling Telegram')
 executor.start_polling(dp, skip_updates=True)
---
+++ tgfuncs.py
@@ -0,0 +1,25 @@
+import confs
+from loguru import logger
+from aiogram.types import Message
+from aiogram import Dispatcher
+
+async def start_cmd(msg: Message):
+	logger.info(f"{msg.from_user.full_name} send /start")
+	await msg.answer("Basic reply")
+
+async def help(msg: Message):
+	logger.info(f"{msg.from_user.full_name} send /help")
+	await msg.answer('This is gate')
+
+async def register():
+	await msg.answer()
+
+async def anything(msg: Message):
+	logger.info(f"{msg.from_user.full_name} send something. {msg['from']['id']} idk")
+	await msg.answer(msg.text)
+
+def setup_handlers(dp: Dispatcher):
+    dp.register_message_handler(start_cmd, commands=['start'])
+    dp.register_message_handler(help, commands=['help'])
+    dp.register_message_handler(register, commands=['register'])
+    dp.register_message_handler(anything)
