5e346783ece867bb99c7e6844054c105ee38871a
Sergievsky Nikita <reetray@protonmail.com>
2021-04-24 10:26:31 +0300

    Добавил отправку сообщений из вк в тг

--- handlers/vk.py
+++ handlers/vk.py
@@ -20,17 +20,22 @@ def setup_tg_bot_to_vk_handler(new_global):
 @simple_bot_message_handler(vk_msg_from_chat,filters.MessageFromConversationTypeFilter("from_chat"))
 async def answer_chat(event: SimpleBotEvent):
     global tg_bot, CONFIG_OBJ
-    #
+
     if event.object.object.message.peer_id not in CONFIG_OBJ['vk']['chats']:
         CONFIG_OBJ['vk']['chats'].append(event.object.object.message.peer_id)
+
     usr_id = event.object.object.message.from_id #event.object.object.message.from_id
     answer = await event.api_ctx.users.get(user_ids=usr_id,fields='first_name,last_name')
     f,l = answer.response[0].first_name, answer.response[0].last_name
-    api_answer = await event.api_ctx.messages.get_conversations_by_id(peer_ids=event.object.object.message.peer_id)
-    chat_title = api_answer.response.items[0].chat_settings.title
-    notification_text = f'Сообщение из беседы {chat_title} от {f} {l}\n> {event.object.object.message.text}'
-    logger.debug(notification_text)
-    await tg_bot.send_message(CONFIG_OBJ['tg']['notificate_to'], notification_text)
+
+    if event.object.object.message.peer_id != CONFIG_OBJ['currentChat']:
+        api_answer = await event.api_ctx.messages.get_conversations_by_id(peer_ids=event.object.object.message.peer_id)
+        chat_title = api_answer.response.items[0].chat_settings.title
+        notification_text = f'Сообщение из беседы {chat_title} от {f} {l}\n{event.object.object.message.text}'
+        await tg_bot.send_message(CONFIG_OBJ['tg']['notificate_to'], notification_text)
+        logger.debug(notification_text)
+    else:
+        await tg_bot.send_message(CONFIG_OBJ['tg']['chat_id'], f'{f} {l}:\n{event.object.object.message.text}')
 
 @simple_bot_message_handler(vk_msg_from_chat,filters.MessageFromConversationTypeFilter("from_pm"))
 async def answer_conv(event: SimpleBotEvent):
@@ -38,8 +43,10 @@ async def answer_conv(event: SimpleBotEvent):
     usr_id = event.object.object.message.from_id #event.object.object.message.from_id
     answer = await event.api_ctx.users.get(user_ids=usr_id,fields='first_name,last_name')
     f,l = answer.response[0].first_name, answer.response[0].last_name
-    notification_text = f'Личное сообщение от {f} {l}\n> {event.object.object.message.text}'
-    logger.debug(notification_text)
-    await tg_bot.send_message(CONFIG_OBJ['tg']['notificate_to'], notification_text)
-
-#async tg_notification(id, )
+    
+    if event.object.object.message.peer_id != CONFIG_OBJ['currentConv'][0]:
+        notification_text = f'Личное сообщение от {f} {l}\n{event.object.object.message.text}'
+        logger.debug(notification_text)
+        await tg_bot.send_message(CONFIG_OBJ['tg']['notificate_to'], notification_text)
+    else:
+        await tg_bot.send_message(CONFIG_OBJ['tg']['conv_id'],f"{event.object.object.message.text}")
